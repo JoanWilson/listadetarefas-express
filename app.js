321e406d73ab0f1ff0fbac31bd1a4378fd9ae958
Joan Wilson <joanwmoliveira@gmail.com>
2021-10-16 14:47:23 -0300

    json

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Olá Mundo, Lista de Tarefas hehe </h1>');
+})
+
+app.get('/json',(req, res) => {
+    res.json({title: 'Tarefa a', done: true});
+})
+
+app.listen(3000, () => {
+    console.log('O servidor foi iniciado');
+})
